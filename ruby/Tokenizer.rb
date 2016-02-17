b761555a2f97ebceb73e92ec0d557fd6e586704b
thestuckster <sstucky3@gmail.com>
2016-02-17 00:40:34 -0600

    some more progress

--- ruby/Tokenizer.rb
+++ ruby/Tokenizer.rb
@@ -1,4 +1,8 @@
-## https://www.youtube.com/watch?v=_EQ5jdrgOlU
+## Reference: https://www.youtube.com/watch?v=_EQ5jdrgOlU
+
+require "TokenType.rb"
+require "TokenData.rb"
+require "Token.rb"
 
 class Tokenizer
 
@@ -14,16 +18,20 @@ class Tokenizer
 
   private
   def define_language()
-    @token_datas.push(TokenData.new(/[a-zA-Z][a-zA-Z0-90]*/), TokenType::INDENTIFIER)
-    @token_datas.push(TokenData.new(/(-)?[0-9]+/), TokenType::INTEGER_LITERAL)
-    @token_data.push(TokenData.new(/".*"/, TokenType::STRING_LITERAL)
+    @token_datas.push(TokenData.new(/^([a-zA-Z][a-zA-Z0-90]*)/), TokenType::INDENTIFIER)
+    @token_datas.push(TokenData.new(/^((-)?[0-9]+))/, TokenType::INTEGER_LITERAL)
+    @token_data.push(TokenData.new(/^(".*")/, TokenType::STRING_LITERAL)
 
-    tokens = [ /:=/, /\\(/ , /\\)/, /\\./, /\\,/]
+    tokens = [ /^(:=)/, /^(\\()/ , /^(\\))/, /^(\\.)/, /^(\\,)/]
 
     tokens.each do |regex|
-      @token_datas.push(TokenData.new(regex, TokenType::TOKEN))
+      @token_datas.push(TokenData.new("^(" << regex << ")", TokenType::TOKEN))
     end
+  end
 
+  public
+  def get_token
+    return @last_token
   end
 
   public
@@ -40,18 +48,38 @@ class Tokenizer
       return @last_token
     end
 
-    @token_datas.each do |regex|
-      if matches = |regex|.match(@str)
+    @token_datas.each do |data|
+      if matches = data.get_pattern().match(@str)
         token = matches.captures
-        ## 19:43
-        @str =
-      end
+        token[0] = ""
+        token = token.join()
 
+        @str = token
 
-    end
+        if data.get_type() == TokenType::STRING_LITERAL
+          @last_token = Token.new(token[1..token.length-1], TokenType::STRING_LITERAL )
+          return @last_token
+        else
+          @last_token = Token.new(token, data.get_type)
+          return @last_token
+        end #if data
+      end#if matches
+    end #each
 
+    raise Exception.new("Could not parse #{@str}")
+
+  end #def
+
+  public
+  def has_next_token?
+    return !(str.empty?)
   end
 
+  public
+  def push_back
+    if @last_token != nil
+      @push_back = true
+    end
+  end
 
-
-end
+end #class

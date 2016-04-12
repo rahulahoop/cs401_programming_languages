7f82a7bb483db7375fdbb7270947f4339a9c559a
thestuckster <sstucky3@gmail.com>
2016-04-12 09:44:28 -0500

    starter code

--- erlang/pi.erl
+++ erlang/pi.erl
@@ -1,3 +1,13 @@
 -module(pi).
--export([]).
+-export([calc/1, calc/3]).
 
+
+%% monte carlo code from source in assignment. Take this and make it use actors.
+calc(Tally) ->
+calc(Tally, 0, 0).
+
+calc(0, Matches, Done) -> 4 * Matches / Done;
+
+calc(Tally, Matches, Done) ->
+X = random:uniform(), Y = random:uniform(),
+calc (Tally – 1, if X*X + Y*Y Matches + 1; true -> Matches end, Done + 1).

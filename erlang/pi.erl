e984570cc28cab6ac6fd307ecc420973df29540d
thestuckster <sstucky3@gmail.com>
2016-04-13 15:33:49 -0500

    scratch

--- erlang/pi.erl
+++ erlang/pi.erl
@@ -1,24 +1,34 @@
 -module(pi).
--export([monte/2]).
-
-%N = iterations X = num actors.
-monte(N, X) ->
-    genActors(X, 0).
-
-
-genActors(X, Y) ->
-    when X > Y ->
-      spawn(uniformGen:gen).
-    genActors(X, Y+1).
-
+-compile(export_all).
+-export([calc/1, calc/3]).
 
 %% monte carlo code from source in assignment. Take this and make it use actors.
 %% Tally is N.
+monte(N, X) ->
+  Matches = [],
+  Tries = .
+
+
+
+%if we calculate a bunch of random numbers less than 1 and put them into a list
+%and then sum the list into one number
+%put that list in Match in the function 4 * matchs / done;
+%this will calculate pi.
 calc(Tally) ->
-calc(Tally, 0, 0).
+  calc(Tally, 0, 0).
+
+calc(Tally, Matches, Done) ->
+  X = random:uniform(), Y = random:uniform(),
+  calc (Tally – 1, if X*X + Y*Y Matches + 1; true -> Matches end, Done + 1).
 
 calc(0, Matches, Done) -> 4 * Matches / Done;
 
-calc(Tally, Matches, Done) ->
-X = random:uniform(), Y = random:uniform(),
-calc (Tally – 1, if X*X + Y*Y Matches + 1; true -> Matches end, Done + 1).
+gen() ->
+  recieve
+    rand ->
+      X = random:uniform(),
+      Y = random:uniform(),
+      if X * X + Y * Y  < 1 -> 1;
+      true -> 0;
+      end.
+  end
